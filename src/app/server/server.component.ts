62bbbd8ee6dc07e751993f1fea8a629f6a13a9b0
github username <60378440+vmar13@users.noreply.github.com>
2021-01-26 20:11:35 -0500

    added html to server comp

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server'
+    selector: 'app-server',
+    templateUrl: './server.component.html'
 })
+
 export class ServerComponent {
 
 }
+
+
