e05af1e59b2f437f68e590673ca884a1079c0b0e
github username <60378440+vmar13@users.noreply.github.com>
2021-02-01 16:50:08 -0500

    testing if revert worked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,14 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule 
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
